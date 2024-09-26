ea2dcb134c0ee16d9416406fbe2c51a9e7413822
nadi726 <16650084+nadi726@users.noreply.github.com>
2024-09-26 21:00:15 +0300

    make outline

---
+++ lib/feedbacks.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+Feedback = Struct.new(:exact, :color_only)
--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,42 @@
 class Game
+  TURNS = 12
+
   def initialize
     @maker = Codemaker.new
     @breaker = Codebreaker.new
     @code = @maker.make
+    @turn = 0
+    @guess_correct = false
   end
 
   def play
+    display_instructions
+    until over?
+      guess = @breaker.guess
+      feedback = check_guess guess # represented as struct: Guess = Struct.new(:exact, :color_only)
+      @breaker.give_feedback feedback
+      @maker.turn_info feedback
+      @turn += 1
+    end
+    on_game_end
+  end
+
+  def over?
+    @turn > TURNS || @guess_correct
+  end
+
+  def check_guess(guess)
+    # TODO
+
+    Feedback.new(0, 0)
+  end
+
+  def display_instructions
+    puts 'Those are the instructions'
+  end
+
+  def on_game_end
+    @breaker.has_won @guess_correct
+    @maker.has_won !@guess_correct
   end
 end
